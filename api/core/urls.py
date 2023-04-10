60af35d12a617e1483f94a7f119acd233aa9c97f
AviaryS <93833608+AviaryS@users.noreply.github.com>
2023-04-10 23:07:23 +0300

    add full crud urls for request and tabs

--- api/core/urls.py
+++ api/core/urls.py
@@ -4,13 +4,22 @@ from . import views
 
 
 urlpatterns = [
-    path('requests/', views.RequestModelViewSet.as_view({'get': 'list'})),
-    path('request/more/<int:pk>/', views.RequestModelViewSet.as_view({'get': 'retrieve'})),
+    path('requests/', views.RequestListOrDetailModelViewSet.as_view({'get': 'list'})),
+    path('request/detail/<int:pk>/', views.RequestListOrDetailModelViewSet.as_view({'get': 'retrieve'})),
+    path('request/create/', views.RequestCreateOrUpdateModelViewSet.as_view({'post': 'create'})),
+    path('request/update/<int:pk>/', views.RequestCreateOrUpdateModelViewSet.as_view({'put': 'update'})),
     path('request/remove/<int:pk>/', views.delete_request_by_pk),
+
     path('request/response/<int:pk>/', views.add_response_to_request),
     path('request/end/<int:pk>/', views.end_request),
+
     path('image/', views.ImageView.as_view()),
     path('image/<int:pk>/', views.ImageView.as_view()),
+
     path('video/', views.VideoView.as_view()),
     path('video/<int:pk>/', views.VideoView.as_view()),
+
+    path('user/detail/<int:pk>/', views.UserDetailModelViewSet.as_view({'get': 'retrieve'})),
+    path('user/update/', views.user_info_update),
+
 ]
