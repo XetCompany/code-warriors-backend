8d54f6e4e7c12b1f0defb66b312a390d2677a58f
AviaryS <93833608+AviaryS@users.noreply.github.com>
2023-04-10 19:58:32 +0300

    check_creator, check_executor

--- api/core/views/request/utils/request.py
+++ api/core/views/request/utils/request.py
@@ -9,3 +9,26 @@ def get_request_object(pk):
     except Request.DoesNotExist:
         raise Http404("Request not found")
     return request_object
+
+
+def check_creator(obj):
+    if obj.creator:
+        creator_id = obj.creator.id
+        creator_username = obj.creator.username
+    else:
+        creator_id = None
+        creator_username = None
+
+    return creator_id, creator_username
+
+
+def check_executor(obj):
+    if obj.executor:
+        executor_id = obj.executor.id
+        executor_username = obj.executor.username
+    else:
+        executor_id = None
+        executor_username = None
+
+    return executor_id, executor_username
+
