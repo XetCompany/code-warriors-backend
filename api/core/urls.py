46e69c8542e1c1b753dafe2e156586d1ad22a9c1
AviaryS <93833608+AviaryS@users.noreply.github.com>
2023-04-10 21:15:02 +0300

    request remove

--- api/core/urls.py
+++ api/core/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('requests/', views.RequestModelViewSet.as_view({'get': 'list'})),
     path('request/more/<int:pk>/', views.RequestModelViewSet.as_view({'get': 'retrieve'})),
+    path('request/remove/<int:pk>/', views.delete_request_by_pk),
     path('request/response/<int:pk>/', views.add_response_to_request),
 ]
