7322a88635fb296ca466d4bc7009ef11f3d17478
AviaryS <93833608+AviaryS@users.noreply.github.com>
2023-04-10 19:19:30 +0300

    delete enter, models.setnull -> cascade, in phonefield default = true

--- app/models.py
+++ app/models.py
@@ -8,11 +8,10 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Request(models.Model):
     """Сведения о запросе на оказание услуг"""
-
     creator = models.ForeignKey(verbose_name='Заказчик', to='User',
-                                on_delete=models.SET_NULL, null=True, related_name='creator')
-    executor = models.ForeignKey(verbose_name='Исполнитель', to='User',
-                                 on_delete=models.SET_NULL, null=True, related_name='executor')
+                                on_delete=models.CASCADE, related_name='creator')
+    executor = models.ForeignKey(verbose_name='Исполнитель', to='User', on_delete=models.SET_NULL,
+                                 blank=True, null=True, related_name='executor')
 
     title = models.CharField(verbose_name='Заголовок запроса', max_length=255)
     description = models.TextField(verbose_name='Описание', blank=True, null=True)
@@ -20,6 +19,9 @@ class Request(models.Model):
     price_to = models.IntegerField(verbose_name='Желаемая цена до', blank=True, null=True)
     deadline_in_days = models.IntegerField(verbose_name='Сроки оказания услуг')
     place = models.CharField(verbose_name='Место оказания услуг', max_length=255)
+
+    responses = models.ManyToManyField(verbose_name='Отклики', to='User', blank=True,
+                                       related_name='responses')
 
     is_active = models.BooleanField(verbose_name='Активен', default=True)
     created_at = models.DateTimeField(verbose_name='Когда создан', auto_now_add=True)
@@ -31,13 +33,13 @@ class Request(models.Model):
         verbose_name_plural = 'Запросы'
 
     def __str__(self):
-        return self.title
+        return f'{self.id} and {self.title}'
 
 
 class Notification(models.Model):
     """Уведомления которая отправляет сообщения и отслеживания прочитал ли пользователь"""
     user = models.ForeignKey(verbose_name='Пользователь', to='User',
-                             on_delete=models.CASCADE, )
+                             on_delete=models.CASCADE)
     message = models.CharField(verbose_name='Сообщение', max_length=255)
     created_at = models.DateTimeField(verbose_name='Время создания', auto_now_add=True)
     is_read = models.BooleanField(verbose_name='Прочитано', default=False)
@@ -56,7 +58,7 @@ class User(AbstractUser):
     username = models.CharField(verbose_name='Логин', max_length=255, unique=True)
     email = models.EmailField(verbose_name='E-mail', max_length=255, unique=True)
     fullname = models.CharField(verbose_name='ФИО', max_length=255, blank=True, null=True)
-    phone = PhoneNumberField(verbose_name='Телефон', blank=True, null=True)
+    phone = PhoneNumberField(verbose_name='Телефон', blank=True, null=True, default=None)
     description = models.TextField(verbose_name='Описание', max_length=255, blank=True, null=True)
 
     notifications = models.ManyToManyField(verbose_name='Уведомления', to=Notification,
