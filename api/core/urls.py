f86802dcc14d9a9ca4e14c6367d53d11d73ec698
AviaryS <93833608+AviaryS@users.noreply.github.com>
2023-04-10 19:59:41 +0300

    show more by request id url

--- api/core/urls.py
+++ api/core/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('requests/', views.RequestModelViewSet.as_view({'get': 'list'})),
+    path('request/more/<int:pk>/', views.RequestModelViewSet.as_view({'get': 'retrieve'})),
     path('request/response/<int:pk>/', views.add_response_to_request),
 ]
