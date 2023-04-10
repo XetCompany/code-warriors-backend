68e4830d96408363d0e10d3e9af04bba81005665
AviaryS <93833608+AviaryS@users.noreply.github.com>
2023-04-10 19:59:17 +0300

    ResponseSerializer, responses for RequestSerializer, add utils

--- api/core/views/request/serializers.py
+++ api/core/views/request/serializers.py
@@ -1,23 +1,36 @@
 from rest_framework import serializers
 
-from app.models import Request
+from api.core.views.request.utils.request import check_executor, check_creator
+from app.models import Request, User
+
+
+class ResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username')
+
 
 class RequestSerializer(serializers.ModelSerializer):
     creator = serializers.SerializerMethodField()
     executor = serializers.SerializerMethodField()
+    responses = ResponseSerializer(many=True)
 
     class Meta:
         model = Request
         fields = '__all__'
 
     def get_creator(self, obj):
+        creator_id, creator_username = check_creator(obj)
+
         return {
-            'id': obj.creator.id,
-            'username': obj.creator.username,
+            'id': creator_id,
+            'username': creator_username,
         }
 
     def get_executor(self, obj):
+        executor_id, executor_username = check_executor(obj)
+
         return {
-            'id': obj.executor.id,
-            'username': obj.executor.username,
+            'id': executor_id,
+            'username': executor_username,
         }
