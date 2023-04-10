ed35aff599ca465586eb3f11e213815aa76805f6
AviaryS <93833608+AviaryS@users.noreply.github.com>
2023-04-10 21:15:24 +0300

    delete_request_by_pk

--- api/core/views/request/views.py
+++ api/core/views/request/views.py
@@ -1,16 +1,19 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from api.core.views.request.serializers import RequestSerializer
-from api.core.views.request.utils.request import get_request_object
+from api.core.views.request.utils.request import get_request_object, check_user_is_creator
 from app.models import Request, Notification
+
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
 class RequestModelViewSet(ModelViewSet):
     queryset = Request.objects.all()
     serializer_class = RequestSerializer
+    permission_classes = [AllowAny]
 
 
 @api_view(['POST'])
@@ -26,3 +29,14 @@ def add_response_to_request(request, pk):
     )
 
     return Response({"data": {"message": "success"}}, status=status.HTTP_201_CREATED)
+
+
+@permission_classes((IsAuthenticated,))
+@api_view(['DELETE'])
+def delete_request_by_pk(request, pk):
+    request_object = get_request_object(pk)
+    check_user_is_creator(request, request_object)
+
+    request_object.delete()
+
+    return Response({"data": {"message": "deleted success"}}, status=status.HTTP_204_NO_CONTENT)
