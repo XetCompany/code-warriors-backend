8fb01ab95282ab7a62f79607911f8cb514b06412
AviaryS <93833608+AviaryS@users.noreply.github.com>
2023-04-10 19:22:02 +0300

    delete paths

---
+++ api/core/views/__init__.py
@@ -0,0 +1,2 @@
+from .request.views import RequestModelViewSet
+from .request.views import add_response_to_request
--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path, include
 
-from . import example_views
-
 urlpatterns = [
-    path('hello/', example_views.hello),
-    path('email/', example_views.send_email),
-
     path('auth/', include('api.auth.urls')),
+
+    path('info/', include('api.core.urls')),
 ]
