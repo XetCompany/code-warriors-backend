3e263428ba5f6fe92f11171169f4073c6b2174d3
XetPy1030 <xetpy1030@mail.ru>
2023-04-10 18:38:18 +0300

    Reset password

--- api/auth/reset_password/urls.py
+++ api/auth/reset_password/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('request_reset_email/', views.request_password_reset_email, name='request_reset_email'),
-    path('password_reset/<token>/', views.password_reset_email, name='password_reset_confirm'),
+    path('request/', views.request_password_reset_email, name='request_reset_email'),
+    path('reset/', views.password_reset_email, name='password_reset_confirm'),
 ]
